f1be16227840c9a16eb4ed2a05bfb69c5869e0a4
Mirek Simek <miroslav.simek@vscht.cz>
2019-07-30 15:32:38 +0200

    Extra options to multilingual

--- invenio_oarepo_multilingual/marshmallow/__init__.py
+++ invenio_oarepo_multilingual/marshmallow/__init__.py
@@ -6,12 +6,12 @@ from marshmallow.fields import List, Nested
 class MultilingualStringPartSchemaV1(StrictKeysMixin):
     """Multilingual string"""
 
-    name = SanitizedUnicode(required=True)
+    value = SanitizedUnicode(required=True)
     lang = SanitizedUnicode(required=True)
 
 
-def MultilingualStringSchemaV1():
-    return Nested(MultilingualStringPartSchemaV1(), many=True)
+def MultilingualStringSchemaV1(**kwargs):
+    return Nested(MultilingualStringPartSchemaV1(), many=True, **kwargs)
 
 
 __all__ = ('MultilingualStringSchemaV1',)
