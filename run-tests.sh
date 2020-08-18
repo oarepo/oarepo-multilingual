219ce7643b5f0f55961d0eba43da41b921592743
Alzbeta Pokorna <alzbeta.pokorna@cesnet.cz>
2020-08-18 16:37:46 +0200

    refactor

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle invenio_oarepo_multilingual tests && \
-#isort -rc -c -df . && \
+isort -rc -c -df . && \
 check-manifest --ignore ".travis-*,docs/_build*" && \
 # sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
