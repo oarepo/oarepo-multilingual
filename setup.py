10629e056528bec2c37945ea3412890c24fe2a03
Alzbeta Pokorna <alzbeta.pokorna@cesnet.cz>
2020-08-26 09:52:21 +0200

    version change

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ extras_require = {
             version=OAREPO_VERSION)],
     'tests-es7': [
         *tests_require,
-        
+
         'oarepo[tests-es7]~={version}'.format(
             version=OAREPO_VERSION)],
 }
