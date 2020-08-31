91bba238dadf33580771f4bd787d3e3aacc7a9de
Alzbeta Pokorna <alzbeta.pokorna@cesnet.cz>
2020-08-31 15:56:11 +0200

    analysis

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle oarepo_multilingual tests && \
-isort -rc -c -df . --gitignore && \
+isort -rc -c -df && \
 check-manifest --ignore ".travis-*,docs/_build*"&& \
 # sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
