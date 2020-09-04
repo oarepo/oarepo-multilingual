384073f7b645cb828d66fb7d4705b3bb9b35f86c
Mirek Simek <miroslav.simek@vscht.cz>
2020-09-04 14:06:38 +0200

    docstrings

--- oarepo_multilingual/config.py
+++ oarepo_multilingual/config.py
@@ -1,3 +1,8 @@
+"""
+Multilingual extension default configuration.
+"""
+
+
 ELASTICSEARCH_DEFAULT_LANGUAGE_TEMPLATE = {
     "type": "text",
     "fields": {
--- oarepo_multilingual/ext.py
+++ oarepo_multilingual/ext.py
@@ -1,13 +1,40 @@
 from oarepo_multilingual import config
 
+
+"""
+Multilingual extension initialization.
+"""
+
+
 class OARepoMultilingualExt:
+    """Multilingual app"""
+
     def __init__(self, app, db=None):
+        """
+        Create an instance of the extension.
+
+        :param app      the application
+        :param db       database, not used
+        """
         self.init_app(app, db=None)
 
     def init_app(self, app, db=None):
+        """
+        Initializes the extension.
+
+        :param app      the application
+        :param db       database, not used
+        """
+
         self.init_config(app)
 
     def init_config(self, app):
+        """
+        Propagates default values to the configuration.
+
+        :param app      the application
+        """
+
         app.config.setdefault(
             'ELASTICSEARCH_DEFAULT_LANGUAGE_TEMPLATE',
             config.ELASTICSEARCH_DEFAULT_LANGUAGE_TEMPLATE
